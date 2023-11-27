35b53c1d706b703ebc7ca3269a592b8babdfdc20
echo <play3step@naver.com>
2023-11-27 17:57:23 +0900

    더미 데이터 설정

---
+++ data/data.js
@@ -0,0 +1,22 @@
+export const musicData = [
+    {
+        title: "Song 1",
+        description: "This is the first song",
+        theme: "Pop"
+    },
+    {
+        title: "Song 1",
+        description: "This is the first song",
+        theme: "Pop"
+    },
+    {
+        title: "Song 1",
+        description: "This is the first song",
+        theme: "Pop"
+    },
+    {
+        title: "Song 1",
+        description: "This is the first song",
+        theme: "Pop"
+    }
+];
