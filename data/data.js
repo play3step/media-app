54bb1700cbbd875ff53686bcf4d0de8cd9b5a87b
echo <play3step@naver.com>
2023-11-28 20:22:44 +0900

    [design] 메인페이지 구현 중

--- data/data.js
+++ data/data.js
@@ -2,21 +2,41 @@ export const musicData = [
     {
         title: "Song 1",
         description: "This is the first song",
-        theme: "Pop"
+        theme: "클래식"
     },
     {
-        title: "Song 1",
+        title: "Song 2",
         description: "This is the first song",
-        theme: "Pop"
+        theme: "클래식"
     },
     {
-        title: "Song 1",
+        title: "Song 3",
         description: "This is the first song",
-        theme: "Pop"
+        theme: "밴드"
     },
     {
-        title: "Song 1",
+        title: "Song 4",
         description: "This is the first song",
-        theme: "Pop"
-    }
+        theme: "밴드"
+    },
+    {
+        title: "Song 5",
+        description: "This is the first song",
+        theme: "발라드"
+    },
+    {
+        title: "Song 6",
+        description: "This is the first song",
+        theme: "발라드"
+    },
+    {
+        title: "Song 7",
+        description: "This is the first song",
+        theme: "힙합"
+    },
+    {
+        title: "Song 8",
+        description: "This is the first song",
+        theme: "힙합"
+    },
 ];
