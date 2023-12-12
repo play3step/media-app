533cb4d73037544d9837b782d5207bebc26789c7
JoEunSang <sang11032@gmail.com>
2023-12-13 00:20:34 +0900

    [faet] update albume list

--- data/data.js
+++ data/data.js
@@ -26,28 +26,28 @@ export const musicData = [
         ],
     },
     {
-        albume_title:'앨범 명',
-        albume_image:'../png/00.png',
+        albume_title:'Car-The Garden-Tree',
+        albume_image:'../png/나무.png',
         songs:[
             {
-                audio: '../musics/시퍼런 봄.mp3',
-                title: '시퍼런 봄',
-                author: '쏜애플(Thornapple)'
+                audio: '../musics/Tree(나무).mp3',
+                title: 'Tree(나무)',
+                author: '카더가든(Car-The Garden)'
             },
             {
-                audio: '../musics/아지랑이.mp3',
-                title: '아지랑이',
-                author: '쏜애플(Thornapple)'
+                audio: '../musics/명동콜링.mp3',
+                title: '명동콜링',
+                author: '카더가든(Car-The Garden)'
             },
             {
-                audio: '../musics/남극.mp3',
-                title: '남극',
-                author: '쏜애플(Thornapple)'
+                audio: '../musics/기다린 만큼, 더..mp3',
+                title: '기다린 만큼, 더.',
+                author: '카더가든(Car-The Garden)'
             },
             {
-                audio: '../musics/백치.mp3',
-                title: '백치',
-                author: '쏜애플(Thornapple)'
+                audio: '../musics/우리의 밤을 외워요.mp3',
+                title: '우리의 밤을 외워요',
+                author: '카더가든(Car-The Garden)'
             }
         ],
     }
